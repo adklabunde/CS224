67090c1406002bc4e917ed6d8554ec94dc292555
Anna Carney <48001097+annacarney@users.noreply.github.com>
2019-03-22 15:27:45 -0500

    Update project.py
    functions to calculate distance and time

--- project.py
+++ project.py
@@ -5,9 +5,27 @@
 #
 # Date: March 11, 2019
 #
+import math
 
 def read_file(filename):
     fn = open(filename, 'r')
     lines = fn.readlines()
     return lines
+
+# takes in the start and end points of two x,y coordinates and returns the distance in meters
+def calc_distance(startpoint, endpoint):
+    x = startpoint[0] - endpoint[0]
+    y = startpoint[1] - endpoint[1]
+    distance = math.sqrt(x**2 + y**2)
+    # convert to kilometers using radius of earth 6371*pi/180 (108)
+    distance = distance * 108
+    # convert to meters
+    distance = distance * 1000
+    return distance
+
+# calculates the amount of time it will take to travel a certain distance in meters
+# 90 meters = 1 minute
+def calc_time(distance):
+    totalminutes = distance / 90
+    return totalminutes
         
