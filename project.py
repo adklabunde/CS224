e12d54557aef8bf9a8da32a7202631e5fa0f9661
Anna Carney <48001097+annacarney@users.noreply.github.com>
2019-04-16 18:38:00 -0500

    Deleted unused methods
    Deleted my calc_distance and calc_time methods that weren't needed

--- project.py
+++ project.py
@@ -5,7 +5,6 @@
 #
 # Date: March 24, 2019
 #
-import math
 from graph import Graph
 from graphics import *
 
@@ -15,23 +14,6 @@ def read_schedule():
     fn = open("students/" + student_num + '/' + day, 'r')
     lines = fn.read().splitlines()
     return lines
-
-# takes in the start and end points of two x,y coordinates and returns the distance in meters
-def calc_distance(startpoint, endpoint):
-    x = startpoint[0] - endpoint[0]
-    y = startpoint[1] - endpoint[1]
-    distance = math.sqrt(x**2 + y**2)
-    # convert to kilometers using radius of earth 6371*pi/180 (108)
-    distance = distance * 108
-    # convert to meters
-    distance = distance * 1000
-    return distance
-
-# calculates the amount of time it will take to travel a certain distance in meters
-# 90 meters = 1 minute
-def calc_time(distance):
-    totalminutes = distance / 90
-    return totalminutes
 
 # The input file for the graph is in the form of:
 # place1 place2 distance
