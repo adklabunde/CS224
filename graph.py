1e519be43ae3cc4c02dc47dddd178c798b7037eb
Aaron <aaron.d.klabunde@gmail.com>
2019-05-01 16:27:12 -0500

    Added dijkstras path algorithm and cleaned up some variable names

--- graph.py
+++ graph.py
@@ -1,37 +1,89 @@
 from collections import defaultdict
+import sys
 
+# Vertex class
+# Holds neighboring verticies and their weights
+class Vertex(object):
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+        self.distance = sys.maxint
+        self.visited = False
+        self.previous = None
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def get_connections(self):
+        return self.adjacent.keys()
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+    def get_distance(self):
+        return self.distance
+
+    def set_distance(self, dist):
+        self.distance = dist
+
+    def set_previous(self, prev):
+        self.previous = prev
+
+    def set_visited(self):
+        self.visited = True
+
+    def set_unvisited(self):
+        self.visited = False
+
+# Graph class
+# Holds weighted vertices
 class Graph(object):
-
     def __init__(self, connections):
-        self._graph = defaultdict(set)
-        self._directed = False
-        self.add_connections(connections)
+        self.vert_dict = {}
+        self.num_vertices = 0
+        for c in connections:
+            self.add_edge(c[0], c[1], c[2])
 
+    def __iter__(self):
+        return iter(self.vert_dict.values())
 
-    def add_connections(self, connections):
-        for node1, node2 in connections:
-            self.add(node1, node2)
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
 
-
-    def add(self, node1, node2):
-        self._graph[node1].add(node2)
-        if not self._directed:
-            self._graph[node2].add(node1)
-
-
-    def find_path(self, start, finish, path=[]):
-        path = path + [start]
-        if start == finish:
-            return path
-        if start not in self._graph:
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
             return None
-        for node in self._graph[start]:
-            if node not in path:
-                new_path = self.find_path(node, finish, path)
-                if new_path:
-                    return new_path
-        return None
 
+    def add_edge(self, frm, to, cost = 0):
+        # Undirected so add edge to/from each vertex
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
 
-    def __str__(self):
-        return '{}({})'.format(self.__class__.__name__, dict(self._graph))
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+    def set_previous(self, current):
+        self.previous = current
+
+    def get_previous(self, current):
+        return self.previous
+
+    def reset_verts(self):
+        verts = self.get_vertices()
+        for v in verts:
+            self.vert_dict[v].set_distance(sys.maxint)
+            self.vert_dict[v].set_previous(None)
+            self.vert_dict[v].set_unvisited()
--- project.py
+++ project.py
@@ -2,28 +2,28 @@
 # CS 224 Spring 2019
 # Project
 #
-#
 # Date: April 19, 2019
 #
 from graph import Graph
 from projectgraphics import *
+import heapq
 
 # Get student number and day of the week
+# Read the schedule from the txt file pertaining to the day provided
 def read_schedule():
     student_num = "student" + raw_input("Student Number: ")
-    user_day = raw_input("Day: ").lower() + ".txt"
-    fn = open("students/" + student_num + '/' + user_day, 'r')
-    lines = fn.read().splitlines()
-    return lines
+    student_day = raw_input("Day: ").lower() + ".txt"
+    fn = open("students/" + student_num + '/' + student_day, 'r')
+    schedule_lines = fn.read().splitlines()
+    return schedule_lines
 
-# The input file for the graph is in the form of:
-# place1 place2 distance
-# where distance is in minutes
+# Read in the graph paths
+# Return a tuple with (start, finish, weight)
 def get_graph():
     fn = open('campus_graph.txt', 'r')
-    lines = fn.read().splitlines()
-    tuples = [(l[0], l[1]) for l in [line.split() for line in lines]]
-    return tuples
+    graph_lines = fn.read().splitlines()
+    path_tuples = [(line[0], line[1], int(line[2])) for line in [path.split() for path in graph_lines]]
+    return path_tuples
 
 # Outputs a graphic window of the campus map image
 def get_window():
@@ -40,18 +40,80 @@ def set_window(path):
     win.getMouse()          # click anywhere in the window to close
     win.close()
 
+# Get the shortest path from the chain of v.previous
+# Append to return list
+# Will be in opposite order
+def get_shortest_path(v, path):
+    if v.previous:
+        path.append(v.previous.get_id())
+        get_shortest_path(v.previous, path)
+    return
+
+# Build the shortest path from start to target
+# Path is save in each vertex
+def build_shortest_path(graph, start, target):
+    # Need to reset the previous and distance values on vertices
+    graph.reset_verts()
+
+    # Set the distance for the start node to zero
+    start.set_distance(0)
+
+    # Put tuple pair into the priority queue
+    unvisited_queue = [(v.get_distance(),v) for v in graph]
+    heapq.heapify(unvisited_queue)
+
+    while len(unvisited_queue):
+        # Pops a vertex with the smallest distance
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        current.set_visited()
+
+        #for next in v.adjacent:
+        for next in current.adjacent:
+            # if visited, skip
+            if next.visited:
+                continue
+            new_dist = current.get_distance() + current.get_weight(next)
+
+            if new_dist < next.get_distance():
+                next.set_distance(new_dist)
+                next.set_previous(current)
+
+        # Rebuild heap
+        # 1. Pop every item
+        while len(unvisited_queue):
+            heapq.heappop(unvisited_queue)
+        # 2. Put all vertices not visited into the queue
+        unvisited_queue = [(v.get_distance(),v) for v in graph if not v.visited]
+        heapq.heapify(unvisited_queue)
+
+
 def main():
     path_list = []
     campus_graph = Graph(get_graph())
-    schedule = read_schedule()
-    student_path = [(p[0], p[1]) for p in [path.split() for path in schedule]]
+    student_schedule = read_schedule()
+    student_path = [(p[0], p[1]) for p in [path.split() for path in student_schedule]]
+
     for path in student_path:
-        user_route = campus_graph.find_path(path[0], path[1])
+        start = campus_graph.get_vertex(path[0])
+        finish = campus_graph.get_vertex(path[1])
+        user_route = [finish.get_id()]
+
+        # Run Dijkstra's shortest path algorithm
+        build_shortest_path(campus_graph, start, finish)
+        get_shortest_path(finish, user_route)
+
+        # Reverse the order of the path
+        user_route = list(reversed(user_route))
+
+        # Save all points on path as ints
         user_route = map(int, user_route)
-        print user_route
+
         path_list.append(user_route)
-    for p in path_list:
-        set_window(p)
+
+    for path in path_list:
+        #print path
+        set_window(path)
 
 if __name__ == '__main__':
     main()
